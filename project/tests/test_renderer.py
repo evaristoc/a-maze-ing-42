1d6cb0064a223a07fd482c5d9d44a341ec9cbe7a
Evaristo Caraballo <ecarabal@f1r5s2.codam.nl>
2026-02-04 22:05:51 +0100

    c:renderer: first successful test of the rendering of the windows

--- project/tests/test_renderer.py
+++ project/tests/test_renderer.py
@@ -1,4 +1,5 @@
 from tests import Maze, ConfigError, ConfigParser
+#from src.collect_config_variables import ConfigParser
 import sys
 
 def main() -> None:
@@ -10,7 +11,7 @@ def main() -> None:
     try:
         config = ConfigParser.from_file(sys.argv[1])
         config = config.config_parser_output_into_dict(config)
-    except ConfigError as error:
+    except Exception as error:
         print(f"Error:\t{error}")
         return
 
@@ -28,12 +29,13 @@ def main() -> None:
 
     engine = Mlx()
     mlx_ptr = engine.mlx_init()
-    win_ptr = engine.mlx_new_window(mlx_ptr, maze_width, maze_height, "win title")
+    print(maze_width, maze_height)
+    win_ptr = engine.mlx_new_window(mlx_ptr, maze_width*10, maze_height*10, "win title")
     engine.mlx_clear_window(mlx_ptr, win_ptr)
     engine.mlx_string_put(mlx_ptr, win_ptr, 20, 20, 255, "Hello PyMlx!")
     (ret, w, h) = engine.mlx_get_screen_size(mlx_ptr)
-
-    # maze = Maze(maze_width, maze_height, seed)
+    engine.mlx_loop(mlx_ptr)
+    maze = Maze(maze_width, maze_height, seed)
 
     # maze.place_fourty_two_glyph_at_maze_center()
     # maze.place_entry_and_exit_cells(config["entry"], config["exit"])
@@ -58,3 +60,26 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+# import sys
+# from pathlib import Path
+
+# # Bepaal het pad naar de hoofdmap van je project (2 mappen omhoog vanaf src/renderer)
+# project_root = Path(__file__).resolve().parent.parent.parent
+
+# # Bouw het pad naar de site-packages in je venv
+# # PAS HIER 'python3.x' AAN naar wat je bij stap 1 zag!
+# venv_path = project_root / ".venv" / "lib" / "python3.12" / "site-packages"
+
+# # Voeg het pad toe aan de Python zoeklijst
+# sys.path.insert(0, str(venv_path))
+
+# # TEST DE IMPORT
+# try:
+#     from mlx import Mlx
+#     print("✅ Succes: Mlx is geladen!")
+#     # Hier kun je je code verder schrijven:
+#     # engine = Mlx()
+# except ImportError as e:
+#     print(f"❌ Fout: Kon 'mlx' niet vinden in {venv_path}")
+#     print(f"Details: {e}")
