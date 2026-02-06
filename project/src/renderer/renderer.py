6c318d5c1b82858d924526a3f109f0794b743389
Evaristo Caraballo <ecarabal@f1r5s6.codam.nl>
2026-02-06 19:28:04 +0100

    c:renderer: adding to renderer.py - singleton, window classes

--- project/src/renderer/renderer.py
+++ project/src/renderer/renderer.py
@@ -1,3 +1,8 @@
+from dataclasses import dataclass
+import sys
+
+
+#@dataclass(frozen=True)
 class ImgData:
     """Structure for image data"""
     def __init__(self):
@@ -9,28 +14,85 @@ class ImgData:
         self.bpp = 0  # bits per pixel
         self.iformat = 0
 
+
+@dataclass
 class XVar:
-    """Structure for main vars"""
-    def __init__(self):
-        self._mlx = None
-        self._mlx_ptr = None
-        self._screen_w = 0
-        self._screen_h = 0
-        self._win_1 = None
-        self._img_1 = ImgData()
+    """Structure for main screen vars using dataclass"""
+    self._mlx = None
+    self._mlx_ptr = None
+    self._screen_w = 0
+    self._screen_h = 0
+
+
+class Screen(XVar):
+    """Screen singleton class"""
+    _instance = None
+    def __new__(cls, mlx_lib: any) -> None:
+        if cls._instance is None:
+            # the singleton is instantiated
+            cls._instance = super(Screen, cls).__new__(cls)
+            # the super is instantiated
+            XVar.__init__(cls._instance)
+            try:
+                # the minilibx is instantiated
+                cls._instance._mlx = mlx_lib
+                # the pointer is instantiated
+                cls._instance._mlx_ptr = cls._instance._mlx.mlx_init()
+                if not cls._instance._mlx_ptr:
+                    raise RuntimeError("mlx_init() failed to return a pointer")
+            except Exception as e:
+                print(f"Error: Can't initialize MLX: {e}", file=sys.stderr)
+                sys.exit(1)
+        return cls._instance
+
+    @property
+    def size(self) -> tuple:
+        return self._mlx.get_screen_size(self._mlx_ptr)
+
+    @property
+    def mlx_ptr(self) -> any:
+        return self._mlx_ptr
+
+
+class XWindowVar:
+    """Structure for window vars"""
+    def __init__(self):        
+        self._win = None
+        self._img = ImgData()
         self._imgidx = 0
 
-class MazeRenderer(XVar):
-    def __init__(self, mlx_instance, cell_size):
-        super().__init__()
-        try:
-            self._mlx = mlx_instance()
-        except Exception as e:
-            print(f"Error: Can't initialize MLX: {e}", file=sys.stderr)
-            sys.exit(1)
-        self._mlx_ptr = self._mlx.mlx.mlx_init() 
-        self._cell_size = cell_size
 
+class Window(XWindowVar):
+    def __init__(self, s: Screen, w: int, h: int, title: str) -> None:
+        super().__init__()        
+        self.engine = Screen()
+        try:
+            self._win = self.engine.mlx_new_window(self.engine.mlx_ptr, w, h, title)
+            if not self._win:
+                raise Exception(f"Can't create {title} window")
+        except Exception as e:
+            print(f"Error: Win create: {e}", file=sys.stderr)
+            sys.exit(1)
+
+class Renderer():
+    def __init__(self, screen: Screen) -> None:
+        window.
+
+        print()
+
+    @classmethod
+    def draw_pixel(self, x: int, w: int, y: int, h: int):
+        for ry in range(y, y + h):
+            for rx in range(x, x + w):
+
+
+
+class CellRenderer(Renderer):
+    def __init__(self):
+        super().__init__()     
+
+    def draw_cell():
+        print()
 # pixel_step = bpp // 8
 
 # for y in range(height):
